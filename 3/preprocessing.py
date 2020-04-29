3861d4f488f418577f01ec9b7104857366c2b373
TheLoooser <ottifant99@hotmail.com>
2020-04-29 21:45:54 +0200

    updated preprocessing to cope with multiple pages
    preprocessing will now create a folder with pictures of words for each page

--- 3/preprocessing.py
+++ 3/preprocessing.py
@@ -1,39 +1,47 @@
 import numpy
+import os
 from PIL import Image, ImageDraw
 from svgpathtools import svg2paths
 
+pages = [270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 300, 301, 302, 303, 304]
+
 # read image as RGB and add alpha (transparency)
-im = Image.open("C:\\Users\\Dizzy\\Desktop\\270.jpg").convert("RGBA")
+for page in pages:
+    im = Image.open("D:\\UniMSc\\PR\\PatRec17_KWS_Data-master\\images\\" + str(page) + ".jpg").convert("RGBA")
 
-# convert to numpy (for convenience)
-imArray = numpy.asarray(im)
+    # convert to numpy (for convenience)
+    imArray = numpy.asarray(im)
 
-polygons, attributes = svg2paths("C:\\Users\\Dizzy\\Desktop\\270.svg")
-nbWords = len(polygons)
+    polygons, attributes = svg2paths("D:\\UniMSc\\PR\\PatRec17_KWS_Data-master\\ground-truth\\locations\\" + str(page)
+                                     + ".svg")
+    nbWords = len(polygons)
 
-index = 0
-for path in polygons:
-    polygon = []
-    for line in path:
-        polygon.append((line.point(0).real, line.point(0).imag))
-    # create mask
-    # polygon = [(444, 203), (623, 243), (691, 177), (581, 26), (482, 42)]
-    maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
-    ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
-    mask = numpy.array(maskIm)
+    index = 0
+    for path in polygons:
+        polygon = []
+        for line in path:
+            polygon.append((line.point(0).real, line.point(0).imag))
+        # create mask
+        # polygon = [(444, 203), (623, 243), (691, 177), (581, 26), (482, 42)]
+        maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
+        ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
+        mask = numpy.array(maskIm)
 
-    # assemble new image (uint8: 0-255)
-    newImArray = numpy.empty(imArray.shape, dtype='uint8')
+        # assemble new image (uint8: 0-255)
+        newImArray = numpy.empty(imArray.shape, dtype='uint8')
 
-    # colors (three first columns, RGB)
-    newImArray[:, :, :3] = imArray[:, :, :3]
+        # colors (three first columns, RGB)
+        newImArray[:, :, :3] = imArray[:, :, :3]
 
-    # transparency (4th column)
-    newImArray[:, :, 3] = mask * 255
+        # transparency (4th column)
+        newImArray[:, :, 3] = mask * 255
 
-    # back to Image from numpy
-    newIm = Image.fromarray(newImArray, "RGBA")
-    newerIm = newIm.crop(newIm.convert("RGBa").getbbox())
-    name = "270-" + str(index)
-    newerIm.save("words\\"+name+".png")
-    index = index + 1
+        # back to Image from numpy
+        newIm = Image.fromarray(newImArray, "RGBA")
+        newerIm = newIm.crop(newIm.convert("RGBa").getbbox())
+        folder = str(page)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        name = str(index)
+        newerIm.save(folder + "\\" + name + ".png")
+        index = index + 1
