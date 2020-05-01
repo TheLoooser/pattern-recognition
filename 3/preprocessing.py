090f5a55d5a3023e39dc887d08213e090f9bbb79
TheLoooser <ottifant99@hotmail.com>
2020-05-01 22:31:52 +0200

    removed transparency and normalised image size

--- 3/preprocessing.py
+++ 3/preprocessing.py
@@ -6,12 +6,14 @@ from svgpathtools import svg2paths
 
 pages = [270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 300, 301, 302, 303, 304]
 
-# read image as RGB and add alpha (transparency)
+# https://stackoverflow.com/questions/22588074/polygon-crop-clip-using-python-pil.
 for page in pages:
+    # read image as RGB and add alpha (transparency)
     # im = Image.open("D:\\UniMSc\\PR\\PatRec17_KWS_Data-master\\images\\" + str(page) + ".jpg").convert("RGBA")  # PIL image
     gray = cv2.imread("D:\\UniMSc\\PR\\PatRec17_KWS_Data-master\\images\\" + str(page) + ".jpg", 0)  # open_cv image
 
     # binarization of PIL image
+    # https://stackoverflow.com/questions/9506841/using-python-pil-to-turn-a-rgb-image-into-a-pure-black-and-white-image
     # if page == 270:
     #     thresh = 125
     #     fn = lambda x: 255 if x > thresh else 0
@@ -19,6 +21,7 @@ for page in pages:
     #     r.save("words\\" + str(index) + "-1.png")
 
     # binarization
+    # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_thresholding/py_thresholding.html
     # im2 = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)  # transform PIL image to open_cv image
     # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # turn image into greyscale image
     # binarization part (different methods)
@@ -61,10 +64,19 @@ for page in pages:
         newIm = Image.fromarray(newImArray, "RGBA")
         newerIm = newIm.crop(newIm.convert("RGBa").getbbox())
 
+        # remove transparency
+        # https://stackoverflow.com/questions/35859140/remove-transparency-alpha-from-any-image-using-pil
+        background = Image.new("RGB", newerIm.size, (255, 255, 255))
+        background.paste(newerIm, mask=newerIm.split()[3])  # 3 is the alpha channel
+
+        # https://stackoverflow.com/questions/1252218/pil-image-resize-not-resizing-the-picture
+        background = background.resize((200, 200), Image.ANTIALIAS)
+
         folder = str(page)
         if not os.path.exists(folder):
             os.makedirs(folder)
         name = str(index)
-        newerIm.save(folder + "\\" + name + ".png")
+        # newerIm.save(folder + "\\" + name + ".png")
+        background.save(folder + "\\" + name + ".png")
         # cv2.imwrite(folder + "\\" + name + ".png", th3)
         index = index + 1
