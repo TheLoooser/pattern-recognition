43a084215a315ba003756917c624977d5ad59909
TheLoooser <ottifant99@hotmail.com>
2020-05-19 19:38:07 +0200

    bipartite graph matching done

--- 5/methods.py
+++ 5/methods.py
@@ -47,14 +47,69 @@ def moleculeEdgeListToDict(edgesList):
         edgesDict[edge[0]] = edge[1]
     return edgesDict
 
-#nr: lenght of molecule
-def moleculeEdgesToCount(nr, edgesList):
+# counts the number of edges of each node (of a single molecule)
+def moleculeEdgesListToEdgeCountList(nr, edgesList):
     count = list()
     for i in range(nr):
-        list.append(sum(x.count(i) for x in edgesList))
+        count.append(sum(x.count(i+1) for x in edgesList))
     return count
 
+# transforms the list of edges into a dictionnary which counts the number of edges of each node (of ever molecule)
+def moleculeEdgesListToEdgeCountDict(nodesList, edgesDict):
+    edgesCountDict = dict()
+    for molecule in nodesList:
+        nr = len(molecule[1])
+        edgesCountDict[molecule[0]] = moleculeEdgesListToEdgeCountList(nr, edgesDict[molecule[0]])
+    return edgesCountDict
+
+
+Cn = 1
+Ce = 1
+# lecture 10 slide 21 (bipartite graph matching)
+def BP(molecule1, molecule2, edges1, edges2):
     
+    #1. build dirac cost matrix
+    matrix = np.matrix(np.ones((len(molecule1)+len(molecule2),len(molecule1)+len(molecule2))) * np.inf)
+    #Upper left: substitutions
+    for i in range(0, len(molecule1)):
+        for j in range(0, len(molecule2)):
+            if not(molecule1[i] == molecule2[j]):
+                val = Ce*abs(edges1[i] - edges2[j]) #number of node deletions/insertions (times the cost)
+                matrix.itemset((i,j),2*Cn + val)            
+            else:
+                val = Ce*abs(edges1[i] - edges2[j]) #number of node deletions/insertions
+                matrix.itemset((i,j), val)
+    #Upper right: deletions
+    for i in range(0, len(molecule1)):
+        val = Cn + Ce*edges1[i] #cost of deleting the node plus number of nodes times cost of deleting all edges of this node
+        matrix.itemset((i,len(molecule2) +i), val)
+    #Lower left: insertions
+    for i in range(0, len(molecule2)):
+        val = Cn + Ce*edges2[i] #cost of inserting the node plus number of nodes times cost of inserting all edges of this node
+        matrix.itemset((len(molecule1)+i,i), val)
+    #Lower right: zeros
+    for i in range(0, len(molecule2)):
+        for j in range(0, len(molecule1)):
+            matrix.itemset((len(molecule1) + i,len(molecule2) + j),0) 
+            
+#     return matrix
+    
+    #2. find optimal assignment (using Hungarian algorithm)
+    #replace infinity with max int
+    matrix = np.where(matrix == np.inf, sys.maxsize, matrix)
+    #Hungarian algorithm (get total cost)
+    matrix = matrix.tolist()
+    m = Munkres()
+    indexes = m.compute(matrix)
+    
+    #3. calculate edit path distance/cost (of the optimal assignment)
+    total = 0
+    for row, column in indexes:
+        value = matrix[row][column]
+        total += value
+    
+    #4. return distance/cost
+    return total
     
     
     
